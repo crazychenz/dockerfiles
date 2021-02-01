c9212a6906e8da767988527fa966be8b142071d5
CrazyChenz <crazychenz@gmail.com>
2021-02-01 15:28:08 -0500

    Adding fork of apt-cacher-ng.

---
+++ apt-cacher-ng/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Origin: https://github.com/sameersbn/docker-apt-cacher-ng
+
+docker build -t $(whoami)/apt-cacher-ng .
---
+++ apt-cacher-ng/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+docker run \
+    --name apt-cacher-ng \
+    --init -d --restart=always --rm \
+    --publish 3142:3142 \
+    --volume /srv/docker/apt-cacher-ng:/var/cache/apt-cacher-ng \
+    $(whoami)/apt-cacher-ng
+
+
+# SELinux users should update the security context of the host mountpoint so that it plays nicely with Docker:
+# 
+# mkdir -p /srv/docker/apt-cacher-ng
+# chcon -Rt svirt_sandbox_file_t /srv/docker/apt-cacher-ng
